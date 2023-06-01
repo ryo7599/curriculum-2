7cad1b0cfc4e5123515cbe36ce3b7e1fc87e7eb4
ryo7599 <110296609+ryo7599@users.noreply.github.com>
2023-06-01 22:17:23 +0900

    課題再提出

---
+++ Javaサンプル/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Java!");
+		public class HelloJava {
+
+		    // フィールド
+		    private String message;
+
+		    // コンストラクタ
+		    public HelloJava() {
+		        message = "Hello, Java!";
+		    }
+
+		    // メソッド
+		    public void print() {
+		        System.out.println(message);
+		    }
+		}
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
+
+
+
+
