9e6f4c2434d4c80db18f4ae9e0c6d9f882b926ef
ryo7599 <110296609+ryo7599@users.noreply.github.com>
2023-07-04 15:53:53 +0900

    10-1　チェックテスト再提出

--- Javaサンプル/10-1/test.js
+++ Javaサンプル/10-1/test.js
@@ -2,11 +2,12 @@ let numbers = [2, 5, 12, 13, 15, 18, 22];
 //ここに答えを実装してください。↓↓↓
 function isEven() {
     for(let i = 0; i<=numbers.length; i++){
-        const num = numbers[i];
-        if(num % 2 == 0){
+        let num = numbers[i];
+        if(num % 2 === 0){
             console.log(num + 'は偶数です');
-        }else{
-            continue;
+        
         }
     }
 }
+
+isEven();
